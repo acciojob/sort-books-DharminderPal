46f856c37610d65ea47bfda92d8e5df0745cc46c
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-08-05 12:23:32 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -21,72 +21,73 @@ const App = () => {
   /*now we have manipulate the dom  using react useffect */
 
   useEffect(() => {
-  fetch("https://www.dbooks.org/api/recent")
-    .then((res) => {
-      if (res.ok) return res.json();
-      else throw new Error("API error");
-    })
-    .then((data) => {
-      data.books.forEach(book => {
-        dispatch(addBook(book));
-      });
-    })
-    .catch(error => console.error('Fetch error:', error));
-}, [dispatch]);
+    fetch("https://www.dbooks.org/api/recent")
+      .then((res) => {
+        if (res.ok) return res.json();
+        else throw new Error("API error");
+      })
+      .then((data) => {
+        data.books.forEach(book => {
+          dispatch(addBook(book));
+        });
+      })
+      .catch(error => console.error('Fetch error:', error));
+  }, [dispatch]);
   //  console.log(books) ok response hai bro 
 
-  const sortedBooks=[...books].sort((a,b)=>{
-    let key=sortKey.toLowerCase()
+  const sortedBooks = [...books].sort((a, b) => {
+    let key = sortKey.toLowerCase()
     const aVal = a[key]?.toLowerCase() || '';
     const bVal = b[key]?.toLowerCase() || '';
-    if(sortOrder==='asc')return aVal.localeCompare(bVal)
-    else if(sortOrder==='dsc')return bVal.localeCompare(aVal)
-  return 0
+    if (sortOrder === 'asc') return aVal.localeCompare(bVal)
+    else if (sortOrder === 'dsc') return bVal.localeCompare(aVal)
+    return 0
   })
-  .slice(0, 15);
+    .slice(0, 15);
   return (
     <div>
       <h1>Books List</h1>
+      <div>
       {/* this is the one part  sort key  */}
-           <label htmlFor='sort' >Sort by:</label>
-      <select id="sort" onChange={(e) => { (setSortKey(e.target.value)) }} >
-        <option value="title">Title</option>
-        <option value="authors">Author</option>
-        <option value="subtitle">Publisher</option>
-      </select>
-  
+      <label htmlFor='sort' >Sort by:
+        <select id="sort" onChange={(e) => { (setSortKey(e.target.value)) }} >
+          <option value="title">Title</option>
+          <option value="authors">Author</option>
+          <option value="subtitle">Publisher</option>
+        </select>
+      </label>
 
 
       {/* this is the part 2nf assecnding order or decensdign order  */}
-      <label htmlFor='orderBy' >Order:</label>
-      <select id="orderBy" onChange={(e) => { (setSortOrder(e.target.value)) }} >
-        <option value="asc">Ascending</option>
-        <option value="dsc">Descending</option>
-      </select>
-  
-    
-<table>
-<thead>
-<tr>
-  <th>Title</th>
-  <th>Author</th>
-  <th>Publisher</th>
-    <th>ISBN</th>
-</tr>
-</thead>
-<tbody>
-{sortedBooks.map((book, index) => (
-  <tr key={index}>
-    <td>{book.title}</td>
-    <td>{book.authors}</td>
-    <td>{book.subtitle}</td>
-    {/* <td><img src={book.image}/></td> */}
-       <td>{book.id}</td>
-  </tr>
-))}
-</tbody>
-  </table>
-    
+      <label htmlFor='orderBy' >Order:
+        <select id="orderBy" onChange={(e) => { (setSortOrder(e.target.value)) }} >
+          <option value="asc">Ascending</option>
+          <option value="dsc">Descending</option>
+        </select>
+      </label>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Publisher</th>
+            <th>ISBN</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedBooks.map((book, index) => (
+            <tr key={index}>
+              <td>{book.title}</td>
+              <td>{book.authors}</td>
+              <td>{book.subtitle}</td>
+              <td>{book.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   )
 }
